f25b8cc014ad6300209f8c438a63934a514ee100
Rollercoasterfewappboy <Rollercoasterfewappboy@outlook.com>
2026-03-01 16:01:31 -0800

    m

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'https://inboxguaranteed.xyz',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
